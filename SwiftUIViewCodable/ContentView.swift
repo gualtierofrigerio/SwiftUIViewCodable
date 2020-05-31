a8e36be13bc1100a39f6fe7456f140d7ede9cda8
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-05-31 17:54:29 +0200

    refactoring ContentView

--- SwiftUIViewCodable/ContentView.swift
+++ SwiftUIViewCodable/ContentView.swift
@@ -9,13 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    var nodes = [ViewNode(type: .group, children: [ViewNode(type: .text, children: []),
-                                                   ViewNode(type: .text, children: [])])]
+    var nodes = [ViewNode(type: .group,
+                          children: ContentView.makeNodes(),
+                          modifiers: [])]
     var viewDecoder = ViewDecoder()
     
     var body: some View {
         viewDecoder.viewsForNodes(nodes)
+    }
+}
+
+extension ContentView {
+    static func makeNodes() -> [ViewNode] {
+        let textNode1 = ViewNode(type: .text, children: [], modifiers: [.font(size: 30)])
+        let textNode2 = ViewNode(type: .text, children: [], modifiers: [.font(size: 10)])
+        return [textNode1, textNode2]
     }
 }
 

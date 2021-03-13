2d5af1cbd3fdc9b591e1d8c3626c8923f8196728
Gualtiero <gualtiero.frigerio@gmail.com>
2021-03-13 14:43:28 +0100

    removed modifiers helper func

--- SwiftUIViewCodable/JSONHelper.swift
+++ SwiftUIViewCodable/JSONHelper.swift
@@ -9,19 +9,6 @@
 import Foundation
 
 class JSONHelper {
-    class func modifiers(fromObject object:Any) -> [Modifier]? {
-        guard let array = object as? [[String:Any]] else {return nil}
-        var modifiers:[Modifier] = []
-        for element in array {
-            if let type = element["type"] as? String,
-                let parameters = element["parameters"] as? [String:Any] {
-                let modifier = Modifier(withType:type, parameters:parameters)
-                modifiers.append(modifier)
-            }
-        }
-        return modifiers
-    }
-    
     class func node(fromData data:Data) -> ViewNode? {
         let decoder = JSONDecoder()
         do {

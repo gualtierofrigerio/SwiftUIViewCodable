402dd2dde04a3248cfb737bacc4c30b689754410
Gualtiero <gualtiero.frigerio@gmail.com>
2021-03-13 14:44:03 +0100

    removed init functions

--- SwiftUIViewCodable/Modifier.swift
+++ SwiftUIViewCodable/Modifier.swift
@@ -14,33 +14,6 @@ enum Modifier {
     case null
 }
 
-extension Modifier {
-    init() {
-        self = .null
-    }
-    
-    init(withType type:String, parameters:[String:Any]) {
-        self = .null
-        switch type {
-        case "background":
-            if  let colorString = parameters["color"] as? String,
-                let color = Modifier.colorFromString(colorString) {
-                self = .background(color: color)
-            }
-        case "font":
-            if let size = parameters["size"] as? CGFloat {
-                self = .font(size: size)
-            }
-        default:
-            self = .null
-        }
-    }
-    
-    static func colorFromString(_ string:String) -> Color? {
-        return Color.yellow
-    }
-}
-
 extension Modifier: Decodable {
     init(from decoder:Decoder) throws {
         let container = try decoder.singleValueContainer()
@@ -58,7 +31,7 @@ extension Modifier: Decodable {
         switch typeString {
         case "background":
             if  let colorString = object["color"],
-                let color = Modifier.colorFromString(colorString) {
+                let color = Color(hexString: colorString) {
                 modifier = .background(color: color)
             }
         case "font":

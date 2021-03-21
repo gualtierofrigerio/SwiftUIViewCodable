539b0e2dd9f90afb912dd41ac2d1ff8aa8ffd7e4
Gualtiero <gualtiero.frigerio@gmail.com>
2021-03-21 18:00:22 +0100

    added comments

--- SwiftUIViewCodable/Modifier.swift
+++ SwiftUIViewCodable/Modifier.swift
@@ -8,12 +8,16 @@
 
 import SwiftUI
 
+/// Enum describing a view modifier
+/// Currently I only support background with a color and font for Text
 enum Modifier {
     case background(color:Color)
     case font(size:CGFloat)
     case null
 }
 
+/// Modifier conforms to Decodable
+/// so we can get a modifier from a piece of JSON
 extension Modifier: Decodable {
     init(from decoder:Decoder) throws {
         let container = try decoder.singleValueContainer()
